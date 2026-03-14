2f61dba23b5ea8ece8b00988553a7a72e9011e5d
Harshvardhan-00012257 <136665792+Harshvardhan-00012257@users.noreply.github.com>
2026-03-14 04:55:58 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+/* Datatypes in JS 
+    Number      =>  Range: Number.MAX_SAFE_INTEGER to Number.MIN_SAFE_INTEGER
+    BigInt      =>  Beyond the scope of number we use BigInt
+    Boolean     =>  True/False
+    String      =>  
+    Symbols     =>  Uniqueness, used in React,figma components (You'll see eventually)
+    null        =>  StandAlone value, It's D.T. is object
+    undefined   =>  Unknown/void
+    objects     =>
+
+    NOTE:
+    Reference documents for JS
+    -   MDN webdocs 
+    -   t39.es.com (Official ecmascript)
+*/
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
