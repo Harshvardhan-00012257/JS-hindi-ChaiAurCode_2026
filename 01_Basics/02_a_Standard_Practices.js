b4d11dcf4fb2162e4fc475f92d13cc2cf4d95194
Harshvardhan-00012257 <136665792+Harshvardhan-00012257@users.noreply.github.com>
2026-03-14 04:33:28 +0000

    Common Standard Practices NOTE

---
+++ 01_Basics/02_a_Standard_Practices.js
@@ -0,0 +1,11 @@
+"Use Strict"; //Strictly Treat all JS code in this file as newer version
+
+
+// alert (3 + 3); //This won't work here as we are executing JS on node, not browser-console (browser is a wrapper around JS-engine)
+
+
+/*Maintian Code Readability*/
+console.log("Sam"             // Bad practice
+    + " Curran"                
+)
+console.log("Sam" + " Curran")   // Better readability
