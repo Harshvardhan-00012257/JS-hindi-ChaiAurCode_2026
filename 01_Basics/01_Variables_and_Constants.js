4fb082c14eefa67f49579ef6df47718316175bb8
Harshvardhan Patil <harshvardhanp2023@gmail.com>
2026-03-25 11:12:12 +0530

    Notes and Observations

--- 01_Basics/01_Variables_and_Constants.js
+++ 01_Basics/01_Variables_and_Constants.js
@@ -1,3 +1,13 @@
+/* 
+Background: 
+        - Software revolve around data - how to fetch, efficiently-store, manipulate data within available memory
+        - Variables are memory spaces allocated by program at runtime.
+        - constants and Variables
+        - Constants: Once declared, cannot be modified.
+          Variables: Two ways to declare in JS, "let" and "var"
+*/
+
+
 /*
     Ways to declare variables in JS.
     "let" and "const" are always preferable
@@ -5,24 +15,26 @@
 */
 
 const accountId = 11223344
+// const accountId2;            // You cannot initialize a constant without any value
+const accountId2 = undefined;        // However, you can initialize a constant with values like undefined, null etc.
 let accountEmail = "BeeBom@gleeBom.com"
 var accountPassword = "98238"
 accountCity = "ShaniShingnapur"
 let accountBalance
 
 /*
-accountId = 55667788    // You cannot reassign a const-variable, node throws error 
+accountId = 55667788            // You cannot reassign a const-variable, node throws error 
 */
 accountEmail = "GleeBom@beeBom.com"
 accountPassword = "5301"
 accountCity = "Jaipur"
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountBalance])
+console.table([accountId, accountId2, accountEmail, accountPassword, accountCity, accountBalance])
 
 accountCity = "Raipur"
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountBalance])
+console.table([accountId, accountId2, accountEmail, accountPassword, accountCity, accountBalance])
 
 /*
     1. "var" variables have issues in differentiating GLOBAL-scope, BLOCK-scope and FUNCTIONAL-scope.
