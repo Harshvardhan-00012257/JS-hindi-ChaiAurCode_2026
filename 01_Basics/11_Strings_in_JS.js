3753e160ebd10c02d6f0036a79300f05530c8a2c
Harshvardhan-00012257 <136665792+Harshvardhan-00012257@users.noreply.github.com>
2026-03-28 03:36:25 +0000

    Strings in JS v2

--- 01_Basics/11_Strings_in_JS.js
+++ 01_Basics/11_Strings_in_JS.js
@@ -11,6 +11,7 @@
 // 4. Get familiar with string methods - Very important (use MDN docs, browser-console etc.)
 //      - String methods (length(), charAt(), indexof(), substring(), slice(), trim(), replace(), includes(), split())
 // 5. Declare strings normally v/s declare string explicitly as object
+//      Explicit declaration strictly-ensures that datatype is "String" (Same for other DT's as well)
 // 6. Do not confuse string as an Object with Arrays
 
 //--------------------------------------+++++++++++++++++++++++++++++++++++++++++++++++----------------------------------------------
